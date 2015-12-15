53849eac89c3261033da19facda3de8bc516bc25
Владислав Ширинкин <vladislav.shirinkin@outlook.com>
2015-12-16 03:34:10 +0500

    lab2

---
+++ lab1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sum1 = 0.4 * 4.0490478732 + 0.4 * computeSum(
+                n => ((n + 1) * n * n * n - (n + 2) * (n * n * n - 0.4)) / ((n * n * n - 0.4) * n * n * n)
+                , 2900);                                                            
+            Console.WriteLine(sum1);
+            Console.Read();
+        }
+
+        static double computeSum(Func<long,double> element, long count)
+        {
+            double sum = 0;  
+            long n = 1;
+            while (n<count)
+            { 
+                sum += element(n);  
+                n++;
+            }                                    
+            return sum;
+        }
+    }
+}
